00f31b4514a6f53c46364f4cb051837b93761106
Aqilla2021 <143184695+Aqilla2021@users.noreply.github.com>
2023-08-26 01:32:03 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,150 @@
+<?php
+session_start();
+error_reporting(0);
+include"config/config.php";
+
+include"config/waktu.php";
+include"config/tgl_indo.php";
+ $resulc = $koneksi->query( "SELECT * FROM kontak WHERE id_kontak = '1'");
+$kontak= mysqli_fetch_assoc($resulc);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'tampilan/head.php';?>
+
+<body>
+
+<!--Header-->
+<?php include"tampilan/menu.php";?>
+<?php
+  include 'tampilan/menu.php';
+ 
+ 
+     if (isset($_GET['page'])) {
+                $page = $_GET['page'];
+                $file = "$page.php";
+
+                if (!file_exists($file)) {
+                    include ("page/home.php");
+                }else{
+                    include ("$page.php");
+                }
+            }else{
+                include ("page/home.php");
+            }
+      
+    ?>
+	
+
+<!-- copyright -->
+<section class="copyright py-4 text-center">
+	<div class="container">
+		<p>© 2018 Design By <a href="" target="=_blank"> KLINIK-PRAKTEK </a></p>
+	</div>
+</section>
+<!-- //copyright -->
+
+<!-- js-scripts -->		
+
+	<!-- js -->
+	<script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script> <!-- Necessary-JavaScript-File-For-Bootstrap --> 
+	<!-- //js -->
+	
+	<!-- desoslide-JavaScript -->
+	<script src="js/jquery.desoslide.js"></script>
+	<script>
+		$('#demo1_thumbs').desoSlide({
+			main: {
+				container: '#demo1_main_image',
+				cssClass: 'img-responsive'
+			},
+			effect: 'sideFade',
+			caption: true
+		});
+	</script>
+
+	<!-- Calendar -->
+		<script src="js/jquery-ui.js"></script>
+		<script>
+			$(function() {
+				$( "#datepicker,#datepicker1" ).datepicker();
+			});
+		</script>
+	<!-- //Calendar -->
+	
+	<!-- banner slider -->
+	<script src="js/responsiveslides.min.js"></script>
+	<script>
+		$(function () {
+			$("#slider4").responsiveSlides({
+				auto: true,
+				pager: true,
+				nav: true,
+				speed: 1000,
+				namespace: "callbacks",
+				before: function () {
+					$('.events').append("<li>before event fired.</li>");
+				},
+				after: function () {
+					$('.events').append("<li>after event fired.</li>");
+				}
+			});
+		});
+	</script>
+	<!-- //banner slider -->
+	
+	<!--pop-up-box -->
+	<script src="js/jquery.magnific-popup.js"></script>
+	<script>
+		$(document).ready(function () {
+			$('.popup-with-zoom-anim').magnificPopup({
+				type: 'inline',
+				fixedContentPos: false,
+				fixedBgPos: true,
+				overflowY: 'auto',
+				closeBtnInside: true,
+				preloader: false,
+				midClick: true,
+				removalDelay: 300,
+				mainClass: 'my-mfp-zoom-in'
+			});
+		});
+	</script>
+	<!-- //pop-up-box -->
+
+	<!-- start-smoth-scrolling -->
+	<script src="js/SmoothScroll.min.js"></script>
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+			});
+		});
+	</script>
+	<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+	<!-- //here ends scrolling icon -->
+	<!-- start-smoth-scrolling -->
+	
+<!-- //js-scripts -->
+
+</body>
+</html>
